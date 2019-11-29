deb4f973591af6765b6accc57d1c3dc228db7662
julbel1984 <julbel1984@yandex.ru>
2019-11-29 18:04:59 +0300

    first commit, add structure mvc

---
+++ config_dist.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'database' => [
+        'name' => '',
+        'username' => '',
+        'password' => '',
+        'connection' => '',
+        'charset' => '',
+        'options' => [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING,
+        ],
+    ],
+];
